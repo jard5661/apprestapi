f9a13b2d8f9f4254276758bd04d6490862270b1d
Fajar Rizkiadi <jard5661@gmail.com>
2022-01-05 00:19:03 +0700

    init ah

---
+++ controller.js
@@ -0,0 +1,31 @@
+'use strict';
+var response = require('./res');
+var connection = require('./koneksi');
+
+exports.index = function(req,res){
+    response.ok('Applikasi berjalan',res);
+}
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res,fields);
+        }
+    });
+};
+
+//menampilkan data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?',[id],
+        function(error, rows, fields){
+            if(error){
+                connection.log(error);
+            }else{
+                response.ok(rows,res,fields);
+            }
+        });
+};
---
+++ routes.js
@@ -0,0 +1,14 @@
+'uses strict';
+
+module.exports = function(app){
+    var jsonku = require('./controller');
+
+    app.route('/')
+        .get(jsonku.index);
+
+    app.route('/tampil')
+        .get(jsonku.tampilsemuamahasiswa);   
+    
+    app.route('/tampil/:id')   
+        .get(jsonku.tampilberdasarkanid); 
+}
